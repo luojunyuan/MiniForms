bf43b7c064fa5cbf8d4e9a20d8bc80460c9a67ec
k1mlka <k1mlka@outlook.com>
2025-07-02 05:24:57 +0800

    Update Application.cs

--- MiniForms.Windows.Forms/Application.cs
+++ MiniForms.Windows.Forms/Application.cs
@@ -19,9 +19,7 @@
         /// </summary>
         public static void Run()
         {
-            // 使用传统的 GetMessage 循环
-            MSG msg = default;
-            while (PInvoke.GetMessage(out msg, HWND.Null, 0, 0) > 0)
+            while (PInvoke.GetMessage(out MSG msg, HWND.Null, 0, 0) > 0)
             {
                 PInvoke.TranslateMessage(msg);
                 PInvoke.DispatchMessage(msg);
@@ -42,7 +40,6 @@
                 // 处理所有待处理的Windows消息，但不阻塞
                 while (PInvoke.PeekMessage(out msg, HWND.Null, 0, 0, PEEK_MESSAGE_REMOVE_TYPE.PM_REMOVE))
                 {
-                    // 检查是否收到退出消息
                     if (msg.message == PInvoke.WM_QUIT)
                     {
                         running = false;
@@ -53,7 +50,6 @@
                     PInvoke.DispatchMessage(msg);
                 }
 
-                // 如果应用程序仍在运行，执行渲染回调
                 if (running && renderCallback != null)
                 {
                     renderCallback();
